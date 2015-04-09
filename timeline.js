ec43250b1c1b4b9b0aa75a6bb315fd028bc832f0
Sam Thorogood <sam.thorogood@gmail.com>
2015-04-09 15:54:25 +1000

    AnimationPlayer -> Animation, seek/playbackRate in past

--- timeline.js
+++ timeline.js
@@ -15,36 +15,60 @@
  */
 
 
+
 /**
+ * FauxTimeline is a manager of many Animation player instances. It is useful
+ * for scheduling and scrubbing a collection of players and related callbacks.
+ *
+ * The timeline emulates the Animation interface, but is not intended to
+ * completely implement it. It supports the playbackRate and currentTime
+ * properties.
+ *
+ * This object expects the Web Animations API to be available either natively
+ * or via polyfill (https://github.com/web-animations/web-animations-js).
+ *
  * @constructor
  */
 var FauxTimeline = function() {
+  if (!('animate' in Element.prototype)) {
+    throw new Error('FauxTimeline expects Web Animations support');
+  }
+
   var timing = {duration: 1000, iterations: Infinity};
   this.hintAtCall_ = this.hintAtCall_.bind(this);
 
   /**
-   * @type {!AnimationPlayer}
+   * @type {!Element}
    * @private
    */
-  this.anim_ = document.body.animate([], timing);
+  this.node_ = document.createElement('div');
 
   /**
-   * @type {number}
+   * @type {!Animation}
    * @private
    */
-  this.localTime_ = 0;
+  this.anim_ = this.node_.animate([], timing);
 
   /**
-   * @type {!Array.<!AnimationPlayer>}
+   * @type {!Array<{offset: number, anim: !Animation}>}
    * @private
    */
   this.players_ = [];
 
   /**
-   * @type {!Array.<{{when: number, fn: function}}>}
+   * @type {!Array<{when: number, fn: !Function}>}
    * @private
    */
   this.calls_ = [];
+
+  /**
+   * Works around issues in some WA implementations. Changing the playbackRate
+   * of an Animation can cause its currentTime to become invalid (null or
+   * undefined). In this case, store the currentTime here before the change.
+   * @type {number}
+   * @private
+   */
+  this.localTime_ = 0;
 };
 
 FauxTimeline.prototype = {
@@ -68,19 +92,42 @@ FauxTimeline.prototype = {
     this.calls_ = this.calls_.slice(i);
   },
 
-  set playbackRate(v) {
-    if (v < 0) {
-      throw new Error('FauxTimeline doesn\'t support <0 playbackRate');
-    }
+  /**
+   * @param {number} playbackRate to set
+   */
+  set playbackRate(playbackRate) {
     this.localTime_ = this.anim_.currentTime;
-    this.anim_.playbackRate = v;
-    this.players_.forEach(function(p) { p.playbackRate = v; });
+    this.anim_.playbackRate = playbackRate;
+    this.players_.forEach(function(p) { p.anim.playbackRate = playbackRate; });
   },
 
+ /**
+  * @return {number}
+  */
   get playbackRate() {
     return this.anim_.playbackRate;
   },
 
+  /**
+   * @param {number} currentTime to set
+   */
+  set currentTime(currentTime) {
+    var seekForward = currentTime > this.currentTime;
+
+    this.anim_.currentTime = currentTime;
+    this.players_.forEach(function(p) {
+      p.anim.currentTime = p.offset + currentTime;
+    });
+
+    if (seekForward) {
+      // For now, we can't trigger calls in the past.
+      this.hintAtCall_();
+    }
+  },
+
+  /**
+   * @return {number}
+   */
   get currentTime() {
     var time = this.anim_.currentTime;
     if (time === null) {
@@ -91,44 +138,32 @@ FauxTimeline.prototype = {
   },
 
   /**
-   * Seek to the specified time. Synchronously runs any registered calls.
-   *
-   * @param {number} to seek to, may not be in past
-   */
-  seek: function(to) {
-    var delta = to - this.currentTime;
-    if (delta < 0) {
-      throw new Error('FauxTimeline doesn\'t support -ve seeks');
-    }
-    this.anim_.currentTime += delta;
-    this.players_.forEach(function(p) { p.currentTime += delta; });
-    this.hintAtCall_();
-  },
-
-  /**
    * Schedule an animation on this FauxTimeline.
    *
    * @param {number} when to start the animation, may be in the past
    * @param {!Element} el to animate
-   * @param {!Array.<!Object>} steps of the animation
-   * @param {number} duration to run for
-   * @return {AnimationPlayer}
+   * @param {!Array<!Object>} steps of the animation
+   * @param {number|!Object} timing to run for
+   * @return {!Animation}
    */
-  schedule: function(when, el, steps, duration) {
+  schedule: function(when, el, steps, timing) {
     var now = this.currentTime;
-    var player = el.animate(steps, duration);
+    var player = el.animate(steps, timing);
 
     player.playbackRate = this.anim_.playbackRate;
     player.currentTime = now - when;
-    this.players_.push(player);
+
+    this.players_.push({anim: player, offset: now - when});
     return player;
   },
 
   /**
-   * Call a function in the future.
+   * Call a function at an absolute time. This must be in the future (even if
+   * the playbackRate is negative), and the call will be cleared once it has
+   * been invoked.
    *
    * @param {number} when to call, must be past currentTime
-   * @param {function} fn to invoke
+   * @param {!Function} fn to invoke
    */
   call: function(when, fn) {
     var now = this.currentTime;
@@ -147,35 +182,38 @@ FauxTimeline.prototype = {
     }
     this.calls_.splice(i, 0, {when: when, fn: fn});
 
-    var player = this.schedule(when, document.body, [], 0);
+    var player = this.schedule(when, this.node_, [], 0);
     player.addEventListener('finish', this.hintAtCall_);
-    return player;
   },
 
   /**
    * Removes a previously registered animation via its AnimationPlayer.
    *
-   * @param {!AnimationPlayer=} opt_player to remove, undefined for all
+   * @param {!Animation=} opt_player to remove, undefined for all
    */
   remove: function(opt_player) {
     if (opt_player === undefined) {
-      this.players_.forEach(function(player) {
-        player.cancel();
-      });
+      this.players_.forEach(function(p) { p.anim.cancel(); });
       this.players_ = [];
       this.calls_ = [];
       return;
     }
 
     if (!('cancel' in opt_player)) {
-      throw new Error('FauxTimeline remove expects AnimationPlayer, was: ' + opt_player);
+      throw new Error('FauxTimeline remove expects Animation, was: ' + opt_player);
     }
     opt_player.cancel();
 
-    var index = this.players_.indexOf(opt_player);
-    if (index > -1) {
-      this.players_.splice(index, 1);
+    var i;
+    for (i = 0; i < this.players_.length; ++i) {
+      var player = this.players_[i];
+      if (player.anim == opt_player) {
+        break;
+      }
     }
+    this.players_.splice(index, 1);
   }
 
 };
+
+window["FauxTimeline"] = FauxTimeline;
